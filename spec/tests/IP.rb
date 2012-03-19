e0f4cf55b7f4a214d418e50b6f08c7940ac52de4
da99 <i-hate-spam-45671204@mailinator.com>
2012-03-19 04:46:08 -0500

    More tests for bin/IP

--- spec/tests/IP.rb
+++ spec/tests/IP.rb
@@ -1,6 +1,19 @@
 
 describe "IP" do
   
+  it "prints xx.xx.xx.xx if file is not found" do
+    chdir {
+      `IP not.found.json`.should == "xx.xx.xx.xx"
+    }
+  end
+
+  it "exists with 1 if not found" do
+    chdir {
+      `IP not.found.json`
+      $?.exitstatus.should == 1
+    }
+  end
+  
   it "returns only one result" do
     chdir {
       `bundle exec IP "*"`.should == "freemont"
