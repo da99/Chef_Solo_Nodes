89a8b61d1fa10261e404c813c8cf9a7aa3be4d55
da99 <i-hate-spam-45671204@mailinator.com>
2012-03-19 04:42:54 -0500

    More tests and fixes for bin/SSH

--- spec/tests/SSH.rb
+++ spec/tests/SSH.rb
@@ -6,6 +6,24 @@ describe "SSH" do
       `SSH "*"`.should == File.basename(Dir.glob("nodes/*.json").first).sub(".json", '')
     }
   end
+
+  it "adds -p argument if port is specified" do
+    chdir {
+      `SSH "with_port"`.should == "-p 2222 ben_and_jerry"
+    }
+  end
+
+  it "adds includes user if \"user\" is specified" do
+    chdir {
+      `SSH "with_user"`.should == "RjD2@localhost"
+    }
+  end
+
+  it "adds user if \"user\" is specified" do
+    chdir {
+      `SSH "with_login"`.should == "log_RjD2@localhost"
+    }
+  end
   
 end # === SSH
 
